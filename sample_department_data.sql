8ac26b654cf7d243cc242daca7a549c5492945a3
Kavitha B G <51849346+Kavitha-BG@users.noreply.github.com>
2022-08-10 21:56:06 +0530

    Add files via upload

--- sample_department_data.sql
+++ sample_department_data.sql
@@ -362,5 +362,284 @@ WHERE salary BETWEEN 31000 AND 33000;
 
 
 
+/* GROUP BY with multiple columns */
+SELECT last_name, department_id, count(*)
+FROM employee
+GROUP BY last_name, department_id
+ORDER BY COUNT(*) DESC ;
+
+SELECT first_name, last_name, count(*)
+FROM employee
+GROUP BY first_name, last_name
+ORDER BY COUNT(*) DESC ;
+
+SELECT first_name, last_name, department_id, COUNT(*)
+FROM employee
+GROUP BY first_name, last_name, department_id
+ORDER BY COUNT(*) DESC ;
+
+
+/* GROUP BY with HAVING*/
+SELECT last_name, COUNT(*)
+FROM employee
+GROUP BY last_name
+HAVING COUNT(*) > 1;
+
+SELECT first_name, last_name, COUNT(*)
+FROM employee
+GROUP BY first_name, last_name
+HAVING COUNT(*) >1;
+
+
+/*SUM function*/
+SELECT SUM(salary)
+FROM employee; 
+
+SELECT SUM(price)
+FROM product;
+
+SELECT department_id, SUM(salary)
+FROM employee
+GROUP BY department_id
+ORDER BY SUM(salary) DESC;
+
+
+/* MAX & MIN function */
+SELECT MAX(salary)
+FROM employee;
+
+SELECT MIN(salary)
+FROM employee;
+
+SELECT MAX(salary), MIN(salary)
+FROM employee;
+
+SELECT department_id, MAX(salary)
+FROM employee
+GROUP BY department_id;
+
+SELECT department_id, MIN(salary)
+FROM employee
+GROUP BY department_id;
+
+SELECT MIN(hire_date)
+FROM employee;
+
+SELECT MAX(hire_date)
+FROM employee;
+
+
+/* AVG function */
+SELECT department_id, AVG(salary)
+FROM employee
+GROUP BY department_id
+ORDER BY AVG(salary);
+
+SELECT AVG(hire_date)
+FROM employee;
+
+
+/* Table aliases */
+SELECT employee_id, first_name, last_name
+FROM employee;
+
+SELECT employee_id, first_name, last_name, e.salary
+FROM employee e
+WHERE e.salary > 30000;
+
+
+/* Column Alias */
+SELECT employee_id, first_name, last_name, e.salary AS "annual salary"
+FROM employee e;
+
+SELECT employee_id, first_name, last_name, e.salary AS "annual_salary", e.salary/12 AS "monthly_salary"
+FROM employee e;
+
+SELECT department_id, COUNT(*) AS employee_count
+FROM employee
+GROUP BY department_id
+ORDER BY department_id;
+
+COMMIT;
+/* Joins */
+SELECT e.employee_id,
+e.first_name,
+e.last_name,
+d.department_name,
+d.department_id,
+e.salary
+FROM employee e
+JOIN department d ON e.department_id = d.department_id
+WHERE e.salary > 60000;
+
+
+/* INNER JOIN */
+SELECT 
+c.customer_id,
+c.first_name,
+c.last_name,
+co.order_date
+FROM customer c
+INNER JOIN customer_order co ON c.customer_id = co.customer_id;
+
+
+/* LEFT OUTER JOIN */
+SELECT 
+d.department_id,
+d.department_name,
+e.employee_id,
+e.first_name,
+e.last_name
+FROM department d 
+LEFT JOIN employee e ON d.department_id = e.department_id 
+ORDER BY d.department_id;
+
+SELECT *
+FROM employee
+WHERE department_id = 9;
+
+/*RIGHT OUTER JOIN */
+SELECT 
+d.department_id,
+d.department_name,
+e.employee_id,
+e.first_name,
+e.last_name
+FROM department d 
+RIGHT JOIN employee e ON d.department_id = e.department_id 
+ORDER BY d.department_id;
+
+
+/*FULL OUTER JOIN */
+SELECT 
+d.department_id,
+d.department_name,
+e.employee_id,
+e.first_name,
+e.last_name
+FROM department d 
+FULL JOIN employee e ON d.department_id = e.department_id 
+ORDER BY d.department_id, e.employee_id ;
+
+/*NATURAL JOIN */
+SELECT 
+e.employee_id,
+e.first_name,
+e.last_name,
+department_id,
+d.department_name
+FROM employee e
+NATURAL JOIN department d;
+
+SELECT product_id,
+product_name,
+department_id,
+department_name
+FROM product
+NATURAL JOIN department;
+
+
+/* CARTESIAN / CROSS JOIN  */
+SELECT employee_id, first_name, last_name
+FROM employee;
+
+SELECT department_id, department_name
+FROM department;
+
+SELECT employee_id, first_name, last_name,
+department_name
+FROM employee, department;
+
+SELECT employee_id, first_name, last_name,
+department_name
+FROM employee 
+CROSS JOIN department;
+
+/* SELF JOIN */
+SELECT emp.employee_id,
+emp.first_name,
+emp.last_name,
+emp.manager_id,
+mgr.first_name,
+mgr.last_name
+FROM employee emp
+LEFT JOIN employee mgr ON emp.manager_id = emp.manager_id;
+
+/* Joining Multiple tables */
+SELECT 
+c.customer_id,
+c.first_name,
+c.last_name,
+c.address_state,
+co.order_id,
+co.order_date,
+p.product_name,
+p.price
+FROM customer c
+JOIN customer_order co ON c.customer_id = co.customer_id 
+JOIN product p ON co.product_id = p.product_id
+ORDER BY co.order_id ;
+
+
+SELECT 
+c.customer_id,
+c.first_name,
+c.last_name,
+c.address_state,
+co.order_id,
+co.order_date,
+p.product_name,
+p.price
+FROM customer c
+LEFT JOIN customer_order co ON c.customer_id = co.customer_id 
+LEFT JOIN product p ON co.product_id = p.product_id
+LEFT JOIN department d ON p.department_id = d.department_id
+ORDER BY c.customer_id ;
+
+
+SELECT order_id,
+customer_id,
+order_id,
+order_date
+FROM customer_order;
+
+SELECT *
+FROM product;
+
+/* ALternative JOIN syntax */
+SELECT employee_id,
+first_name,
+last_name,
+department_name
+FROM employee, department
+WHERE employee.department_id = department.department_id;
+
+/* employee.department_id = department.department_id(+) LEFT OUTER JOIN*/
+/* employee.department_id (+)= department.department_id RIGHT OUTER JOIN*/
+/* employee.department_id(+) = department.department_id(+) */
+
+SELECT employee_id,
+first_name,
+last_name,
+department_name,
+salary,
+hire_date
+FROM employee, department
+WHERE salary > 50000
+AND hire_date > '01-01-2012'
+AND employee.department_id = department.department_id;
+
+
+
+
+
+/* STRING FUNCTIONS */
+SELECT customer_id,
+first_name,
+last_name,
+email_address,
+INSTR(email_address, '@' )
+FROM customer;
+
 
 
